c0fc8826aace707d133985efe53f9a2889716098
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-09-10 04:57:01 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-single digit numbers of base 10
+ *
+ * Result: 0 (success)
+ */
+int main(void)
+{
+	int base;
+
+	for (base = '0' ; base <= '9' ; base++)
+		putchar(base);
+	putchar('\n');
+	return (0);
+}
