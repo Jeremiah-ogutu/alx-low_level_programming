0970bb3abbfa0b3d0b7f3a96a64ca89e09969470
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-09-10 02:40:56 -0700

    printf

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,16 @@
-#incled <stdio.h>
+#include <stdio.h>
+/**
+ * main-print alphabet
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char j
+		for (j = "a"; j <= "z"; j++){
+			putchar(j);
+		}
+	putchar("\n");
+
+		return (0);
+}
