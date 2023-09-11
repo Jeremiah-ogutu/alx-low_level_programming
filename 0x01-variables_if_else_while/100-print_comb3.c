8b29c0949767846661f059083af95cea287771ea
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-09-11 01:51:37 -0700

    1-last_digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main-Write a program that prints all possible different combinations of two digits.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	for(int i = 0; i < 9; i++)
+	{
+		for(int j = i+1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			 if (i != 8 || j != 9)
+			 {
+				  putchar(',');
+				  putchar(' ');
+			 }
+		}
+	}
+	 putchar('\n');
+
+    return 0;
+}
