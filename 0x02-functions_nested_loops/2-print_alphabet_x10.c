d97a792b86127c957919d70fbd5e0cbaa8ceed49
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-09-12 05:00:26 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- a function that prints 10 times alphabet
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int numberOfTimes;
+	char alphabet;
+
+	for (numberOfTimes = 0 ; numberOfTimes <= 9 ; numberOfTimes++)
+	{
+		for  (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+			_putchar(alphabet);
+	}
+	_putchar('\n');
+}
+
+
