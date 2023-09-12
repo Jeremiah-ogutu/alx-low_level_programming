60346066450a68bb6616670add4e72c4e26a1836
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-09-12 11:43:18 -0700

    timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table -a function that prints the 9 times table, starting with 0.
+ *
+ */
+void times_table(void)
+{
+	int row;
+	int column;
+	int result;
+
+	for (row = 0 ; row < 10 ; row++)
+	{
+		for (column = 0 ; column < 10 ; column++)
+		{
+			result = row * column;
+			printf("%4d ", result);
+		}
+		printf("\n");
+	}
+}
