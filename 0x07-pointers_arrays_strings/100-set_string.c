d4bac81f5ec13e171d53bdcc835f3cc351f61430
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-09-26 01:48:14 -0700

    101-crackme_password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ *set_string-fuctio that set the value of a string
+ *@s:the pointer
+*@to:char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
