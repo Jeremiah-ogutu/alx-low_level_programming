60f6f607d64f0f25e399b63844989ac550186aed
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-10-02 08:26:00 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main- the starting point
+ * @argc: counter
+ * @argv:pointer
+ * Return:0 success
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int j;
+	int num;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[i]);
+		if (num > 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
