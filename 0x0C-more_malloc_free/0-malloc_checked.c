0f798279ed8a89503a03ed1ac1960c2c29b456ed
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-10-05 02:18:11 -0700

    task 0-malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,16 @@
 #include "main.h"
 /**
  * malloc_checked-fuction that allocate memory using maloc
- * @b:it a prototype
+ * @b:it alocate memory size
  * Return:pointer
  */
+void *malloc_checked(unsigned int b)
+{
+	char *pointer;
+
+	pointer = malloc(b);
+	if (pointer == NULL)
+		exit(98);
+	else
+		return (pointer);
+}
