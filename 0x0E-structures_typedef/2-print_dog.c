30c5361195fe0956fda3946031e8f00e8473aef1
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-10-10 05:21:59 -0700

    change of return fun

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,6 @@ void print_dog(struct dog *d)
 	{
 		printf("name: %s\n", (d->name) ? d->name : "(nil)");
 		printf("age: %f\n", (d->age) ? d->age : 0);
-		printf("Owner: %s\n", (d->owner) ? d->owner : "(NULL)");
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil");
 	}
 }
