a3ae0ed1d469ef8b6c151cb3a4a7d4702c4613fd
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-10-27 12:36:54 +0300

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,2 +1,25 @@
 #include "main.h"
 
+/**
+ * clear_bit - function that set value to bit 0 at agiven inde
+ * @n:pointer of unsigned int
+ * @index: the given index
+ *
+ * Return:1 success else -1 
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	mask = ~mask;
+
+	*n &= mask;
+
+	return (1);
+}
